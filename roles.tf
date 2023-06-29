1530eba27f144053d260d0be1fcb56bfea342f91
hendrixzack <129194411+hendrixzack@users.noreply.github.com>
2023-06-29 14:40:37 -0400

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -28,7 +28,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
   role       = aws_iam_role.cluster.name
 }
-resource "aws_iam_role" "node" {
+resource "aws_iam_role" "node1" {
   name = "skillstorm-nodes"
 
   assume_role_policy = jsonencode({
@@ -45,17 +45,17 @@ resource "aws_iam_role" "node" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.node1.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.node1.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.node1.name
 }
 
 
