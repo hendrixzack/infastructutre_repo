b299bdbe7b5830c3b65ac9ab1daf69e3be0dc6a6
hendrixzack <129194411+hendrixzack@users.noreply.github.com>
2023-06-29 14:39:12 -0400

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -33,17 +33,4 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 
 
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.name
-}
 
-resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node.name
-}
